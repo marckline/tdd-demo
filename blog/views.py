ed579c80b8b3e1ebb0167f0411fb598b7e977b3f
Marc Kline <marc.kline@byjakt.com>
2015-11-20 14:38:23 -0500

    Mistake in view test

--- blog/views.py
+++ blog/views.py
@@ -2,8 +2,9 @@ from django.shortcuts import render
 from rest_framework.viewsets import ModelViewSet
 
 from .models import Post
+from .serializers import PostSerializer
 
 class PostViewSet(ModelViewSet):
     queryset = Post.objects.all()
-    
+    serializer_class = PostSerializer
 
