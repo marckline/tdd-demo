67dd38da65c9e4c850fb2724f1352d96dcb3be73
Marc Kline <marc.kline@byjakt.com>
2015-11-20 13:36:17 -0500

    Add migrations for tests

---
+++ blog/migrations/0001_initial.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Author',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
+                ('position', models.CharField(max_length=64, blank=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Post',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=255)),
+                ('author', models.ForeignKey(to='blog.Author')),
+            ],
+        ),
+    ]
--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -2,7 +2,7 @@ import pytest
 
 from .factories import PostFactory, AuthorFactory
 
-
+@pytest.mark.django_db
 def test_post_must_have_author():
     post = PostFactory()
     post.author = None
