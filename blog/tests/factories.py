4af17e72b6741448bf02b08f34b3463d501aff3c
Marc Kline <marc.kline@byjakt.com>
2015-11-20 12:13:44 -0500

    Factories created, no models

---
+++ blog/tests/factories.py
@@ -0,0 +1,19 @@
+import factory
+
+
+class AuthorFactory(factory.DjangoModelFactory):
+    name = "Some Guy"
+    position = "Head Honcho"
+
+    class Meta:
+        model = Author
+
+
+class PostFactory(factory.DjangoModelFactory):
+    title = "Marketers won't believe these lead generation techniques for marketing marketing to marketers"
+    author = factory.SubFactory(AuthorFactory)
+
+    class Meta:
+        model = Post
+
+
---
+++ blog/tests/test_models.py
@@ -0,0 +1,8 @@
+import pytest
+
+from .factories import PostFactory, AuthorFactory
+
+
+
+
+
