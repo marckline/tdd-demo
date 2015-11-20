404630f4a2e27d6ccd600e837c4f45e2f470f1a2
Marc Kline <marc.kline@byjakt.com>
2015-11-20 14:20:48 -0500

    Post view test written; fails

---
+++ blog/tests/test_views.py
@@ -0,0 +1,21 @@
+import pytest, factory
+
+from django.core.urlresolvers import reverse
+from rest_framework.test import APITestCase
+
+from .factories import PostFactory
+
+class PostViewTests(APITestCase):
+
+    def test_post_list(self):
+
+        for i in range(0,1):
+            post = PostFactory()
+            post.save()
+
+        url = reverse('post-list')
+        response = self.client.get(url)
+
+        assert len(response.data) == 2
+
+
